e1f98da8d1d6056da712664c3c74d37da7ad6182
mwcodebase <mwaldron.personal@gmail.com>
2020-03-21 17:31:37 -0400

    skeleton update

--- app.py
+++ app.py
@@ -2,6 +2,33 @@ from bs4 import BeautifulSoup
 from requests import get as get_request
 from re import compile as compile_regex
 
+def get_commute(url):
+  return "Not implemented yet"
+
+def extract_substring(string, regex):
+  return "Not implemented yet"
+
+def collect_information(posting):
+  info = posting.find('div', class_='info')
+    
+  name = "Not implemented yet"
+  price = info.find('div', class_='price')
+  address = info.find('span', class_='address')
+  commute_time = "Not implemented yet"
+  move_in = "Not implemented yet"
+  move_out = "Not implemented yet"
+  link = "Not implemented yet"
+
+  return f"""
+  Posting name: {name}
+  Rent: {price}
+  Property address: {address}
+  Commute time: {commute_time}
+  Move-in date: {move_in}
+  Move-out date: {move_out}
+  Link to page: {link}
+  """
+
 if __name__ == "__main__":
   # sadly, https://ochdatabase.umd.edu/, doesn't have an API, but there is a degree of consistency to search queries and their matching URLs
   # the simplest way forward is to build a search manually and then copy/paste the URL below, as we have done
@@ -14,22 +41,7 @@ if __name__ == "__main__":
   postings = search_results.find_all('article', class_=compile_regex(r'^ocp-property-search property-\d?.*'))
 
   parsed_posts = []
-  for post in postings:
-    name = 
-    price = 
-    address = 
-    #commute_time = TODO?
-    move_in = 
-    move_out = 
-    link = 
+  for posting in postings:
+    parsed_posts.append(collect_information(posting))
 
-    parsed_posts.append(f"""
-    Posting name: {name}
-    Rent: {price}
-    Property address: {address}
-    Move-in date: {move_in}
-    Move-out date: {move_out}
-    Link to page: {link}
-    """)
-
-  print(parsed_posts)
+  print("\n".join(parsed_posts))
